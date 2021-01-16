065be49d8120413d5f1f3a7bc22ca8498539ad24
michelandzela <77416519+michelandzela@users.noreply.github.com>
2021-01-16 16:24:37 +0200

    Naujas20210116

---
+++ ManoBaigiamasis/Pages/MainPage.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace AutoPaskaitos.ManoBaigiamasis.Pages
+{
+    class MainPage : BasicPages
+    {
+        public MainPage(IWebDriver driver) : base(driver) { }
+
+        private IWebElement logInButton => driver.FindElement(By.CssSelector("#masthead > div.header-inner.flex-row.container.logo-center.medium-logo-center > div.flex-col.hide-for-medium.flex-right > ul > li.account-item.has-icon > a > i"));
+        private IWebElement emailBox => driver.FindElement(By.Id("username"));
+        private IWebElement password => driver.FindElement(By.Id("password"));
+        private IWebElement logIn => driver.FindElement(By.Name("login"));
+        private IWebElement faultEmail => driver.FindElement(By.CssSelector(".woocommerce-error > li"));
+        private IWebElement wrongPassword => driver.FindElement(By.CssSelector(".woocommerce-error > li"));
+        
+        public void PressLogInButton()
+        {
+            logInButton.Click();
+        }
+        public void WriteCorrectEmail()
+        {
+            string email = "j.michalcenko@gmail.com";
+            emailBox.SendKeys(email);
+        }
+        public void WriteCorrectPassword()
+        {
+            string mypassword = "AutoTestavimas2020";
+            password.SendKeys(mypassword);
+        }
+        public void PressPrisijungti()
+        {
+            logIn.Click();
+        }
+        public void WriteBadEmail()
+        {
+            string bademail = "something";
+            emailBox.SendKeys(bademail);
+        }
+        public void WriteBadPassword()
+        {
+            string badpassword = "badpassword";
+            password.SendKeys(badpassword);
+        }
+        public void CheckFault()
+        {
+            Assert.AreEqual("KLAIDA: Netinkamas vartotojo vardas. Pamiršote slaptažodį?", faultEmail.Text);
+        }
+        public void CheckWrongPassword()
+        {
+            Assert.AreEqual("KLAIDA: Įvestas slaptažodis el pašto adresui j.michalcenko@gmail.com yra neteisingas. Pamiršote slaptažodį?", wrongPassword.Text); // del sitos
+        }
+        public void CheckLogOutButton()
+        {
+            Assert.IsNotNull(driver.FindElement(By.CssSelector("#masthead > div.header-inner.flex-row.container.logo-center.medium-logo-center > div.flex-col.hide-for-medium.flex-right > ul > li.account-item.has-icon.has-dropdown > ul > li.woocommerce-MyAccount-navigation-link.woocommerce-MyAccount-navigation-link--customer-logout > a")));
+        }
+    }
+}
---
+++ ManoBaigiamasis/Tests/Basic.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace AutoPaskaitos.ManoBaigiamasis.Tests
+{
+    class Basic
+    {
+        public IWebDriver driver;
+
+        [SetUp]
+
+        public void BeforeEveryTest()
+        {
+            driver = new ChromeDriver();
+            driver.Url = "https://www.biomedikoscentras.lt/";
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+       
+        [TearDown]
+        public void AfterTest()
+        {
+            driver.Quit();
+        }
+    }
+}
